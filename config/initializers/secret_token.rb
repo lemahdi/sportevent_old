894a888a5f4fd0264c306a03fb4fe823a8b7f4db
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-04-27 09:41:28 +0200

    La Yolette Parisienne project, start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Layolette::Application.config.secret_key_base = 'c85bc942300e654a2a3783ee39048090cdbad2cd5ba5ac2aa47a6eee73830603f1ce7f6c8c99528f4d781d3e364629a72423ca48d2325f7987740a7993b82fd9'
