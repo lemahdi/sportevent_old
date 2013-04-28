9dc0f6469a0ee9920021847020114632e5f75733
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-04-28 10:38:23 +0200

    changing rameurs viewer

--- app/helpers/rameurs_helper.rb
+++ app/helpers/rameurs_helper.rb
@@ -1,2 +1,9 @@
 module RameursHelper
+
+  # Returns the Gravatar (http://gravatar.com/) for the given rameur
+  def gravatar_for(rameur)
+    gravatar_id = Digest::MD5::hexdigest(rameur.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: rameur.email, class: "gravatar")
+  end
 end
