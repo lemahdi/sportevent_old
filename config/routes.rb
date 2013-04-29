4e5974530135cb34ce354fc953cae3f5f4f063a3
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-04-29 22:03:04 +0200

    adding session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Layolette::Application.routes.draw do
   get "home/index"
   resources :rameurs
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,6 +12,8 @@ Layolette::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get '/signup' => 'rameurs#new'
+  get '/signin' => 'sessions#new'
+  match '/signout' => 'sessions#destroy', via: :delete
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
