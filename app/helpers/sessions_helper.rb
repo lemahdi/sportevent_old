98b660ce03456eba7336524c5b415ecaa0251daa
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-04-30 23:17:50 +0200

    including the sessions helper into the controller because the default behaviour is to hide it from the controller and to make it visible from the viewer

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+	def sign_in(rameur)
+		cookies.permanent[:remember_token] = rameur.remember_token
+		self.current_rameur = rameur
+	end
+
+	def signed_in?
+		!current_rameur.nil?
+	end
+
+	def current_rameur=(rameur)
+		@current_rameur = rameur
+	end
+
+	def current_rameur
+		@current_rameur ||= Rameur.find_by_remember_token(cookies[:remember_token])
+	end
 end
