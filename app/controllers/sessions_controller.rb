0806b70939a0a607b96b63e149432371705c62fb
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-04-30 21:34:25 +0200

    session controller, writing routes and viewers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+    rameur = Rameur.find_by_email(params[:session][:email].downcase)
+    respond_to do |format|
+      if rameur && rameur.authenticate(params[:session][:password])
+    		# Sign the rameur in and redirect to the rameur's show page.
+        format.html { redirect_to rameur }
+        format.json { render action: 'show', status: :created, location: rameur }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: rameur.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def destroy
