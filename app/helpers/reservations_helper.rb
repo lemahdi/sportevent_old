62ea9b0344d63b2b91d4a22d9e70c9c03e549d10
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-05-07 00:27:16 +0200

    getting creneaus and avirons descriptions from db and displaying them in a drop-down select helper

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,17 @@
 module ReservationsHelper
+	
+	def aviron_types
+		Aviron.uniq(:nbplaces).order("nbplaces ASC").map { |aviron| aviron.description }
+	end
+
+	def creneau_times
+		simple_double = Creneau.order('debut ASC').map do |creneau|
+			["#{creneau.debut.strftime('%H:%M')}-#{creneau.fin.strftime('%H:%M')}", creneau.id] if creneau.fin-creneau.debut == 45.minutes
+		end.select { |c| c }
+		yolette = Creneau.order('debut ASC').map do |creneau|
+			["#{creneau.debut.strftime('%H:%M')}-#{creneau.fin.strftime('%H:%M')}", creneau.id] if creneau.fin-creneau.debut == 1.hour
+		end.select { |c| c }
+
+		[["horaires simple et double", nil]] + simple_double + [["horaires yolette", nil]] + yolette
+	end
 end
