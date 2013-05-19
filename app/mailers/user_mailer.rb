5544fd053559baeea39bbb4ccf69143a804e899c
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-05-20 00:02:19 +0200

    adding welcome page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def welcome_email(rameur)
+  	@user = rameur
+  	@url = "localhost:3000/signin"
+  	mail(to: rameur.email, subject: "Bienvenue à la Yolette Parisienne")
+  end
 end
