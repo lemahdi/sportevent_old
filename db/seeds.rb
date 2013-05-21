b262f7f838b30539c42bae6f3679b0cb487590b9
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-05-22 01:33:29 +0200

    DB poulation script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Aviron model
+Aviron.create(description: "simple", nbplces: 1)
+Aviron.create(description: "double", nbplces: 2)
+Aviron.create(description: "yolette", nbplces: 4)
+
+# Creneau model
+## 45 minutes
+t = Time.now.change(hour: 9, min: 0, sec: 0)
+(1..4).each do |i|
+	Creneau.create(debut: t, fin: t+45.minutes)
+	t = t + 45.minutes
+end
+t = t.change(hour: 14, min: 0)
+(1..4).each do |i|
+	Creneau.create(debut: t, fin: t+45.minutes)
+	t = t + 45.minutes
+end
+## 1 hour
+t = t.change(hour: 9, min: 0)
+(1..3).each do |i|
+	Creneau.create(debut: t, fin: t+1.hour)
+	t = t + 1.hour
+end
+t = t.change(hour: 14, min: 0)
+(1..3).each do |i|
+	Creneau.create(debut: t, fin: t+1.hour)
+	t = t + 1.hour
+end
