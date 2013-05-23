0a3ec7ca2ecf3f86dc69ad02a3a6c7dbdbe2388b
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-05-23 20:44:30 +0200

    actionmailer settings for heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,11 +9,11 @@ ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.default content_type: "text/html"
 
 ActionMailer::Base.smtp_settings = {
-  address:              "smtp.gmail.com",
+  address:              Rails.env.production? ? "smtp.sendgrid.net" : "smtp.gmail.com",
   port:                 587,
-  domain:               "gmail.com",
-  user_name:            "mahdi00",
-  password:             "paris0",
+  domain:               Rails.env.production? ? "heroku.com" : "gmail.com",
+  user_name:            Rails.env.production? ? ENV['SENDGRID_USERNAME'] : "mahdi00",
+  password:             Rails.env.production? ? ENV['SENDGRID_PASSWORD'] : "paris0",
   authentication:       :plain,
   enable_starttls_auto: true
 }
