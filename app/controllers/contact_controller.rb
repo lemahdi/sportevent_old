b30215474e7acf2ed121438878150dac12549492
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-05-29 00:46:16 +0200

    signing in is not required in order to contact the administrator

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,5 @@
 class ContactController < ApplicationController
-  before_filter :signed_in_rameur, only: [:new, :create, :update]
+  before_filter :signed_in_rameur, only: :update
 
   def new
   	@contact = Contact.new
