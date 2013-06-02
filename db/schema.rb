fba55682aae4b79cbd58913e2df1d40775ec3213
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-06-02 19:01:23 +0200

    missed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130602124308) do
+ActiveRecord::Schema.define(version: 20130602151325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,11 +35,8 @@ ActiveRecord::Schema.define(version: 20130602124308) do
     t.string   "prenom"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin",                  default: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -51,7 +48,6 @@ ActiveRecord::Schema.define(version: 20130602124308) do
   end
 
   add_index "rameurs", ["email"], name: "index_rameurs_on_email", unique: true
-  add_index "rameurs", ["remember_token"], name: "index_rameurs_on_remember_token"
   add_index "rameurs", ["reset_password_token"], name: "index_rameurs_on_reset_password_token", unique: true
 
   create_table "registres", force: true do |t|
